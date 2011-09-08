58d1cb66813f799c5523b025bb78765f2670c878
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2011-09-08 16:16:27 +0200

    changed license to CC0

--- pcm.h
+++ pcm.h
@@ -1,3 +1,10 @@
+/*
+robot36 - encode and decode images using SSTV in Robot 36 mode
+Written in 2011 by <Ahmet Inan> <xdsopl@googlemail.com>
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
 
 #ifndef PCM_H
 #define PCM_H
