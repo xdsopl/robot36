04e68c262f042093ba2f9366f87871d0e4b2a76a
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2011-09-08 13:44:43 +0200

    moved window functions into own files

--- decode.c
+++ decode.c
@@ -8,6 +8,7 @@
 #include <time.h>
 #include "mmap_file.h"
 #include "pcm.h"
+#include "window.h"
 
 float lerp(float a, float b, float x)
 {
@@ -28,48 +29,6 @@ float limit(float min, float max, float x)
 {
 	float tmp = x < min ? min : x;
 	return tmp > max ? max : tmp;
-}
-
-float sinc(float x)
-{
-	return 0 == x ? 1.0 : sinf(M_PI * x) / (M_PI * x);
-}
-float hann(float n, float N)
-{
-	return 0.5 * (1.0 - cosf(2.0 * M_PI * n / (N - 1.0)));
-}
-float hamming(float n, float N)
-{
-	return 0.54 - 0.46 * cosf(2.0 * M_PI * n / (N - 1.0));
-}
-float lanczos(float n, float N)
-{
-	return sinc(2.0 * n / (N - 1.0) - 1.0);
-}
-float gauss(float n, float N)
-{
-	float o = 0.35;
-	return expf(- 1.0/2.0 * powf((n - (N - 1.0) / 2.0) / (o * (N - 1.0) / 2.0), 2.0));
-}
-
-float i0f(float x)
-{
-	// converges for -3*M_PI:3*M_PI in less than 20 iterations
-	float sum = 1.0, val = 1.0, c = 0.0;
-	for (int n = 1; n < 20; n++) {
-		float tmp = x / (2 * n);
-		val *= tmp * tmp;
-		float y = val - c;
-		float t = sum + y;
-		c = (t - sum) - y;
-		sum = t;
-	}
-	return sum;
-}
-float kaiser(float n, float N)
-{
-	float a = 2.0;
-	return i0f(M_PI * a * sqrtf(1.0 - powf((2.0 * n) / (N - 1.0) - 1.0, 2.0))) / i0f(M_PI * a);
 }
 
 typedef struct {
---
+++ window.h
@@ -0,0 +1,12 @@
+
+#ifndef WINDOW_H
+#define WINDOW_H
+float sinc(float);
+float hann(float, float);
+float hamming(float, float);
+float lanczos(float, float);
+float gauss(float, float);
+float kaiser(float, float);
+
+#endif
+
