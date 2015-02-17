007dbb0a42a399148cc1da80da0ee842c5a6993d
Ahmet Inan <xdsopl@googlemail.com>
2015-02-17 15:41:42 +0100

    set decoder member to null onDestroy, so it gets out of scope

--- app/src/main/java/xdsopl/robot36/MainActivity.java
+++ app/src/main/java/xdsopl/robot36/MainActivity.java
@@ -136,6 +136,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy () {
         decoder.destroy();
+        decoder = null;
         manager.cancel(notifyID);
         super.onDestroy();
     }
