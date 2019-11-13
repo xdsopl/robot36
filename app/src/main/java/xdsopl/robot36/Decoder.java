55f3d8ae75bf9f30189ef7ec6fe5b3617da687d1
Ahmet Inan <xdsopl@gmail.com>
2019-11-13 15:38:07 +0100

    refactored for AndroidX

--- app/src/main/java/xdsopl/robot36/Decoder.java
+++ app/src/main/java/xdsopl/robot36/Decoder.java
@@ -20,9 +20,9 @@ import android.graphics.Bitmap;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
+import androidx.renderscript.Allocation;
+import androidx.renderscript.Element;
+import androidx.renderscript.RenderScript;
 
 public class Decoder {
     private boolean drawImage = true, quitThread = false;
