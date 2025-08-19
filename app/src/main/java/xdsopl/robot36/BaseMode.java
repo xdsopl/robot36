5aa3be6cc986c4a7bb859cd56dcfef1cd5e07e11
Marek Ossowski <marek0ossowski@gmail.com>
2025-08-19 23:19:26 +0200

    Fixed indents to tabs

--- app/src/main/java/xdsopl/robot36/BaseMode.java
+++ app/src/main/java/xdsopl/robot36/BaseMode.java
@@ -3,8 +3,8 @@ package xdsopl.robot36;
 import android.graphics.Bitmap;
 
 public abstract class BaseMode implements Mode {
-    @Override
-    public Bitmap postProcessScopeImage(Bitmap bmp) {
-        return Bitmap.createScaledBitmap(bmp, bmp.getWidth() / 3, bmp.getHeight() / 3, true);
-    }
+	@Override
+	public Bitmap postProcessScopeImage(Bitmap bmp) {
+		return Bitmap.createScaledBitmap(bmp, bmp.getWidth() / 3, bmp.getHeight() / 3, true);
+	}
 }
